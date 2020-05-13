7e04ed86a9b29bd67832166e9ab3103caa9ce754
Brett Larney <93brett@gmail.com>
2020-05-13 18:34:16 -0400

    changed to use file extension, add filename to csv

--- fender.py
+++ fender.py
@@ -7,17 +7,17 @@ def main():
 
     with open('fenderTEST.csv', 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        spamwriter.writerow(['Amplifier', 'Name', 'Author', 'Rating'])
+        spamwriter.writerow(['Amplifier', 'Name', 'Author', 'Rating', 'Filename'])
 
         for f in os.listdir('.'):
-          if re.match('preset', f):
+          if f.endswith(".fuse"):
             tree = ET.parse(f)
             root = tree.getroot()       
             for fuse in root.findall('FUSE'):
                 name = fuse.find('Info').attrib['name']
                 author = fuse.find('Info').attrib['author']
                 rating = fuse.find('Info').attrib['rating']
-            spamwriter.writerow([root.attrib['amplifier'], name, author, rating])
+            spamwriter.writerow([root.attrib['amplifier'], name, author, rating, f])
    
 if __name__ == "__main__":
     main();
