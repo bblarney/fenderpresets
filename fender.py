e34199fa90c4897cb4fd94715d765e196c01bc5a
Brett Larney <93brett@gmail.com>
2020-05-18 13:39:19 -0400

    added a couple more fields, changed csv filename to match dir, added small post log

--- fender.py
+++ fender.py
@@ -2,22 +2,33 @@ import csv
 import xml.etree.ElementTree as ET
 import os, re
 
-
 def main():
 
-    with open('fenderTEST.csv', 'w', newline='') as csvfile:
+    with open((os.path.basename(os.getcwd())+'.csv'), 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        spamwriter.writerow(['Amplifier', 'Name', 'Author', 'Rating', 'Filename'])
+        spamwriter.writerow(['Amplifier', 'Name', 'Author', 'Rating', 'Tags', 'Filename', 'Info'])
+        passed = 0
+        failed = 0
 
         for f in os.listdir('.'):
           if f.endswith(".fuse"):
-            tree = ET.parse(f)
-            root = tree.getroot()       
-            for fuse in root.findall('FUSE'):
-                name = fuse.find('Info').attrib['name']
-                author = fuse.find('Info').attrib['author']
-                rating = fuse.find('Info').attrib['rating']
-            spamwriter.writerow([root.attrib['amplifier'], name, author, rating, f])
+            try:
+                tree = ET.parse(f)
+                root = tree.getroot()       
+                for fuse in root.findall('FUSE'):
+                    name = fuse.find('Info').attrib['name']
+                    author = fuse.find('Info').attrib['author']
+                    tags = fuse.find('Info').attrib['tags']
+                    rating = fuse.find('Info').attrib['rating']
+                    info = fuse.find('Info').text
+                    
+                spamwriter.writerow([root.attrib['amplifier'], name, author, rating, tags, f, info])
+                passed+=1
+            except:
+                failed+=1
+                pass
+
+    print(passed , " files processed, " , failed ,  "  files failed.")
    
 if __name__ == "__main__":
-    main();
+    main()
